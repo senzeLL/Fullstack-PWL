a5979110fdb071b81e27e7cdefb50f23a01ff07b
senzeLL <benedicandrea37@gmail.com>
2022-12-21 17:03:59 +0700

    tugas pwl fullstack

---
+++ react-client/src/components/home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return(
+        <div>
+            <h1>HOME - Benedictus</h1>
+            <h5>/register untuk daftar atau /login untuk login</h5>
+        </div>
+    )
+}
+
+export default Home
